e9f456bbc2c3ba4a2e3d3dd459bb32ae37b384a9
HackTheBridge <110546197+HackTheBridge@users.noreply.github.com>
2022-10-08 14:47:24 +1000

    Update zonetransfer_host.py

--- zonetransfer_host.py
+++ zonetransfer_host.py
@@ -3,10 +3,11 @@ import os
 
 domain = input("Please input a domain name: ")
 
+# Creates a file of identified name servers to read from.
 NS_file = os.system("host -t NS " + domain + " | awk '{print $4}' | sed 's/\.$//' > " + domain + "_NS.txt")
 
+# Reads the file line by line and attempts a zone transfer for each nameserver.
 f = open((domain + "_NS.txt"), "r")
-
 for line in f:
 	print("")
 	print("#" * 50)
@@ -14,7 +15,6 @@ for line in f:
 	print("Attempting zone transfer for " + line)
 	print("#" * 50)
 	os.system("host -l " + domain + " " + line)
-
 f.close()
 	
 	
